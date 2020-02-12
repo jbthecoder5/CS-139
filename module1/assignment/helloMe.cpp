93e13ddb5a099347f2a635bb58269188c9cfb57a
Joseph Bourne <josephpbourne@gmail.com>
2020-02-12 12:26:11 -0800

    created my module 1 assignment

---
+++ module1/assignment/helloMe.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string userName;
+
+    // ask the user for his name
+    cout << "What is your full name? ";
+
+    // input the username
+    cin >> userName;
+
+    // output the users full name
+    cout << "Your full name is: " << userName << endl;
+}
