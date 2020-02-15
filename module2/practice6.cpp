dd8ec1101f50a8fa7a9660935733c912b56a8dd5
Joseph Bourne <josephpbourne@gmail.com>
2020-02-15 11:23:07 -0800

    another practice c++ program file

---
+++ module2/practice6.cpp
@@ -0,0 +1,16 @@
+// yet another well-adjusted printing program
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cin.get();
+
+    cout << "The following items were top sellers\n";
+    cout << "during the month of June:\n";
+    cout << "Coffee\n";
+    cout << "Aspirin\n";
+
+    return 0;
+}
