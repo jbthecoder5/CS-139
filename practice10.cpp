776437e9bc960efd19d6ab1f951034f198c9f910
Joseph Bourne <josephpbourne@gmail.com>
2020-02-17 13:12:52 -0800

    added another file for my c++ class in module 2 for practice learning about character literals

---
+++ practice10.cpp
@@ -0,0 +1,17 @@
+// this program works with characters in c++
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.get();
+
+    char letter;
+
+    letter = 'A';
+    cout << letter << endl;
+    letter = 'B';
+    cout << letter << endl;
+
+    return 0;
+}
