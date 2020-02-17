cdda425fe77f7495394f81268a43f6e7ca68d18c
Joseph Bourne <josephpbourne@gmail.com>
2020-02-17 15:00:59 -0800

    added another practice file for my c++ programming class

---
+++ module2/practice11.cpp
@@ -0,0 +1,16 @@
+// importing the string class
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cin.get();
+
+    string movieTitle;
+
+    movieTitle = "Wheels of Fury";
+    cout << "My favorite movie is " << movieTitle << endl;
+    return 0;
+}
