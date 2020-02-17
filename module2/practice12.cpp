371341e369dd7a6428ed83cb6bb1ffed1ab1cc51
Joseph Bourne <josephpbourne@gmail.com>
2020-02-17 15:52:57 -0800

    added another c++ file to my repo

---
+++ module2/practice12.cpp
@@ -0,0 +1,18 @@
+// this program uses floating point data types.
+// joseph bourne
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.get();
+
+    float distance;
+    double mass;
+
+    distance = 1.495979E11;
+    mass = 1.989E30;
+    cout << "The Sun is " << distance << " meters away.\n";
+    cout << "The Sun\'s mass is " << mass << " kilograms.\n";
+    return 0;
+}
