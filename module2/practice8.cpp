2e6e81751fa17121effcb0a7dcf0667f0210b3a3
Joseph Bourne <josephpbourne@gmail.com>
2020-02-17 13:00:54 -0800

    added another file for my c++ class in module 2 for practice

---
+++ module2/practice8.cpp
@@ -0,0 +1,22 @@
+// this program has variables of several of the integer types.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.get();
+
+    int checking;
+    unsigned int miles;
+    long days;
+
+    checking = -20;
+    miles = 4276;
+    days = 189000;
+    cout << "We have made a long journey of " << miles;
+    cout << " miles.\n";
+    cout << "Our checking account balance is " << checking;
+    cout << "\nAbout " << days << " days ago Columbus ";
+    cout << "stood on this spot.\n";
+    return 0;
+}
