6a9f612f690d927ba1f88ab16a28c1457e9c590f
Joseph Bourne <josephpbourne@gmail.com>
2020-02-17 13:04:21 -0800

    added another file for my c++ class in module 2 for practice

---
+++ module2/practice9.cpp
@@ -0,0 +1,19 @@
+// a c++ program using 3 variables on the same line.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.get();
+
+    int floors, rooms, suites;
+
+    floors = 15;
+    rooms = 300;
+    suites = 30;
+
+    cout << "The Grande Hotel has " << floors << " floors\n";
+    cout << "with " << rooms << " rooms and " << suites;
+    cout << " suites.\n";
+    return 0;
+}
