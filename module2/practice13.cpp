7ed0eb2e2cbe41f734235cc9dce77df642dbd382
Joseph Bourne <josephpbourne@gmail.com>
2020-02-18 07:57:12 -0800

    another c++ practice file for module 2. almost done with this chapter

---
+++ module2/practice13.cpp
@@ -0,0 +1,12 @@
+// this program shows variables initialization.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.get();
+    int month = 2, days = 28;
+
+    cout << "Month " << month << " has " << days << " days.\n";
+    return 0;
+}
