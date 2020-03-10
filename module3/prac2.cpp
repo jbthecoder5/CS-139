7c0066a518cc7acbc903ba8f2e627b90eafa3e5b
Joseph P. Bourne <josephpbourne@gmail.com>
2020-03-09 20:01:53 -0700

    more practice files

---
+++ module3/prac2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.get();
+    int length, width, area;
+
+    cout << "This program calculates the area of a ";
+    cout << "rectangle.\n";
+    cout << "Enter the length and width of the rectangle ";
+    cout << "separated by a space.\n";
+    cin >> length >> width;
+    area = length * width;
+    cout << "The area of the rectangle is " << area << endl;
+    return 0;
+}
