eafc048edc2f082760099133335b3e0d8ec90ccc
Joseph P. Bourne <josephpbourne@gmail.com>
2020-03-24 19:22:58 -0700

    added module5 for my c++ programming class

---
+++ module5/prac1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.get();
+
+    int num = 4;
+
+    cout << "The variable num is " << num << endl;
+    cout << "I will now increment num.\n\n";
+
+    num++;
+    cout << "Now the variable num is " << num << endl;
+    cout << "I will increment num again.\n\n";
+
+    ++num;
+    cout << "Now the variable num is " << num << endl;
+    cout << "I will now decrement num.\n\n";
+
+    num--;
+    cout << "Now the variable num is " << num << endl;
+    cout << "I will decrement num again.\n\n";
+
+    --num;
+    cout << "Now the variable num is " << num << endl;
+
+    return 0;
+}
