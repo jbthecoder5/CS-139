ada1cfe38661ce8829d1c3f39edd058501e9a594
Joseph P. Bourne <josephpbourne@gmail.com>
2020-03-25 01:23:13 -0700

    another file adding to module 5

---
+++ module5/prac2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cin.get();
+
+    int num = 4;
+
+    cout << num << endl;
+    cout << num++ << endl;
+    cout << num << endl;
+    cout << ++num << endl;
+    cout << endl;
+
+    cout << num << endl;
+    cout << num-- << endl;
+    cout << num << endl;
+    cout << --num << endl;
+
+    return 0;
+}
