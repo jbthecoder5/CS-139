5ff3423f6951b0600b98e2a6ced8d4c6dc30b489
Joseph P. Bourne <josephpbourne@gmail.com>
2020-04-06 12:42:08 -0700

    another file added to module6

---
+++ module6/prac2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void displayMessage() {
+    cout << "Hello from the function displayMessage.\n";
+}
+
+int main() {
+    cin.get();
+
+    cout << "Hello from main.\n";
+    for (int count = 0; count < 5; count++)
+        displayMessage();
+    cout << "Back in function main again.\n";
+    return 0;
+}
