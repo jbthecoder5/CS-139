67e0a3306425c3459eb5cfcf2726083a9e38221e
Joseph P. Bourne <josephpbourne@gmail.com>
2020-04-08 04:54:29 -0700

    working on module assignment early

---
+++ module6/assignments/assignment6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.get();
+
+    return 0;
+}
+
+void getJudgeData() {
+
+}
+
+void calcScore() {
+
+}
+
+double findLowest() {
+
+}
+
+double findHighest() {
+    
+}
