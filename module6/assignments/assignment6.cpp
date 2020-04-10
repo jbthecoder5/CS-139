e61aee80b8bce0cab420a53404374098281e5aa4
Joseph P. Bourne <josephpbourne@gmail.com>
2020-04-10 00:31:53 -0700

    forgot to initialize the void and double functions

--- module6/assignments/assignment6.cpp
+++ module6/assignments/assignment6.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void getJudgeData();
+void calcScore();
+double findLowest();
+double findHighest();
+
+
 int main() {
     cin.get();
-
+    
     return 0;
 }
 
 void getJudgeData() {
-
+    
 }
 
 void calcScore() {
@@ -16,9 +22,9 @@ void calcScore() {
 }
 
 double findLowest() {
-
+    return 0;
 }
 
 double findHighest() {
-    
+    return 0;
 }
